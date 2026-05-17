cc38652481f28fafdfda0e9a408adc8bef49deaf
Karo4a <t1mati666@yandex.ru>
2026-05-17 21:41:08 +0300

    add SecurityHelper

---
+++ DocumentGenerator.Services/Helpers/SecurityHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Cryptography;
+
+namespace DocumentGenerator.Services.Helpers
+{
+    /// <summary>
+    /// Хелпер по работе с криптографией
+    /// </summary>
+    public class SecurityHelper
+    {
+        private const byte MaximumBytes = 32;
+        private const byte MaximumIterations = 6;
+
+        /// <summary>
+        /// Генерировать соль в 32 байта
+        /// </summary>
+        public static string GenerateSalt32() => GenerateSalt(MaximumBytes);
+
+        /// <summary>
+        /// Генерировать соль заданного размера
+        /// </summary>
+        public static string GenerateSalt(int nSalt)
+        {
+            using var provider = RandomNumberGenerator.Create();
+            var salt = new byte[nSalt];
+            provider.GetNonZeroBytes(salt);
+
+            return Convert.ToBase64String(salt);
+        }
+
+        /// <summary>
+        /// Получить солированный хэш пароля в 32 байта
+        /// </summary>
+        public static string HashPassword32(string password, string salt)
+            => HashPassword(password, salt, MaximumIterations, MaximumBytes);
+
+        /// <summary>
+        /// Получить солированный хэш пароля
+        /// </summary>
+        public static string HashPassword(string password, string salt, int nIterations, int nHash)
+        {
+            var saltBytes = Convert.FromBase64String(salt);
+            return Convert.ToBase64String(Rfc2898DeriveBytes.Pbkdf2(password, saltBytes, nIterations, HashAlgorithmName.SHA256, nHash));
+        }
+    }
+}
